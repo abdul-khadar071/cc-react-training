5af16ae9a4febc8fc7e3d4ed5e584eebfa359b46
Abdul khadar <62008481+abdul-khadar071@users.noreply.github.com>
2021-09-28 20:46:28 +0530

    Add files via upload

---
+++ session6/render/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Renderlist from './components/renderlist';
+//import Grocery from './components/Grocery';
+//import Contact from './components/Contact';
+ function App() {
+  
+  return (
+    <div className="App">
+<Renderlist/> 
+  {/* <Grocery/>  */}
+  {/* <Contact/>   */}
+    </div>
+  );
+}
+
+export default App;
+
+
