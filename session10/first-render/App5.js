3c1a117d48c02352559a753107d7cd0d64fc279f
Abdul khadar <62008481+abdul-khadar071@users.noreply.github.com>
2021-10-11 21:23:36 +0530

    Add files via upload

--- session10/first-render/App5.js
+++ session10/first-render/App5.js
@@ -1,1 +1,31 @@
+import { useEffect, useRef, useState } from "react";
+ 
+function App5() {
 
+  const [count, setCount] = useState(0);
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    console.log(count);
+  });
+
+  const increment=() => {
+    setCount((count) => count + 1);
+  }
+  return (
+    <center><div>
+      <p>{count}</p>
+      <button
+        onClick={increment}
+      >
+       +
+      </button>
+     
+    </div></center>
+  );
+}
+
+export default App5;
