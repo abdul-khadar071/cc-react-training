d481a883a07cc7b1de45e13924d8dec2a9c04fed
Abdul khadar <62008481+abdul-khadar071@users.noreply.github.com>
2021-10-18 19:32:30 +0530

    Add files via upload

---
+++ session11/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Search = ({onSearch}) => {
+    return (
+        <div>
+        <label htmlFor="search">Search for :</label> 
+        <input id="search" type="text" onChange={onSearch}/>
+        
+       </div>
+    )
+}
+
+export default Search
+
+
